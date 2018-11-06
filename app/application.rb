dc2100a85d7dc72b2047dbd8e177b7e09dda242c
Marc Mills <mdmills1990@gmail.com>
2018-11-06 22:15:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,9 +26,9 @@ class Application
       if @@items.include? item_to_add
         @@cart << item_to_add
         resp.write "added #{item_to_add}\n"
-      else 
+      else
         resp.write "We don't have that item\n"
-      
+      end
     else
       resp.write "Path not found"
     end
